a96ebe458d7295150978ef983c886013b4fa45f6
Max Nasalevich <maks20061999@gmail.com>
2019-02-19 16:13:09 +0300

    add toast in feature_2

--- app/src/main/java/com/example/epamandroidtest/MainActivity.java
+++ app/src/main/java/com/example/epamandroidtest/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(getApplicationContext(), "toast feature_1", Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), "toast feature_2", Toast.LENGTH_LONG).show();
     }
 }
