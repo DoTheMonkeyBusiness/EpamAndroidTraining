d37977139372a78dd2e596d8be12ee92299eaf11
Max Nasalevich <maks20061999@gmail.com>
2019-02-19 17:18:04 +0300

    change button text
    (cherry picked from commit f07d93f9992237ba6df3f48e232341ac77696b1c)

--- app/src/main/java/com/example/epamandroidtest/MainActivity.java
+++ app/src/main/java/com/example/epamandroidtest/MainActivity.java
@@ -4,17 +4,20 @@ import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView text;
+    private Button button1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        text = (TextView)findViewById(R.id.textView1);
+        text = findViewById(R.id.textView1);
+        button1 = findViewById(R.id.button1);
         Toast.makeText(getApplicationContext(), "toast feature_1", Toast.LENGTH_LONG).show();
         Toast.makeText(getApplicationContext(), "second toast feature_1", Toast.LENGTH_LONG).show();
         Toast.makeText(getApplicationContext(), "third toast feature_1", Toast.LENGTH_LONG).show();
@@ -25,12 +28,6 @@ public class MainActivity extends AppCompatActivity {
         text.setText(R.string.myText);
 
 
-
+        button1.setText(R.string.button1_new_text);
     }
-
-
-
-
-
-
 }
