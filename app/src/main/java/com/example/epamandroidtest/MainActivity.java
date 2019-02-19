165f3ea51dea90f036612d966e35a63341d6742c
Max Nasalevich <maks20061999@gmail.com>
2019-02-19 17:14:02 +0300

    fix bug with textView

--- app/src/main/java/com/example/epamandroidtest/MainActivity.java
+++ app/src/main/java/com/example/epamandroidtest/MainActivity.java
@@ -8,13 +8,13 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView textView;
+    private TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = (TextView)findViewById(R.id.textView);
+        text = (TextView)findViewById(R.id.textView1);
         Toast.makeText(getApplicationContext(), "toast feature_1", Toast.LENGTH_LONG).show();
         Toast.makeText(getApplicationContext(), "second toast feature_1", Toast.LENGTH_LONG).show();
         Toast.makeText(getApplicationContext(), "third toast feature_1", Toast.LENGTH_LONG).show();
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(getApplicationContext(), "second toast feature_2", Toast.LENGTH_LONG).show();
         Toast.makeText(getApplicationContext(), "third toast feature_2", Toast.LENGTH_LONG).show();
 
-        textView.setText(R.string.textView);
+        text.setText(R.string.myText);
 
 
 
