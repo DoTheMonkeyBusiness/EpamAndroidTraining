f07d93f9992237ba6df3f48e232341ac77696b1c
Max Nasalevich <maks20061999@gmail.com>
2019-02-19 17:18:04 +0300

    change button text

--- app/src/main/java/com/example/epamandroidtest/MainActivity.java
+++ app/src/main/java/com/example/epamandroidtest/MainActivity.java
@@ -2,20 +2,25 @@ package com.example.epamandroidtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        button1 = (Button)findViewById(R.id.button1);
         Toast.makeText(getApplicationContext(), "toast feature_1", Toast.LENGTH_LONG).show();
         Toast.makeText(getApplicationContext(), "second toast feature_1", Toast.LENGTH_LONG).show();
         Toast.makeText(getApplicationContext(), "third toast feature_1", Toast.LENGTH_LONG).show();
         Toast.makeText(getApplicationContext(), "toast feature_2", Toast.LENGTH_LONG).show();
         Toast.makeText(getApplicationContext(), "second toast feature_2", Toast.LENGTH_LONG).show();
         Toast.makeText(getApplicationContext(), "third toast feature_2", Toast.LENGTH_LONG).show();
+
+        button1.setText(R.string.button1_new_text);
     }
 }
