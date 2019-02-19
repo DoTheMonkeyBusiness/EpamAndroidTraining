5d362b8caa0921d475da0d69339ec8e65ff2570b
Max Nasalevich <maks20061999@gmail.com>
2019-02-19 16:24:21 +0300

    add textView

--- app/src/main/java/com/example/epamandroidtest/MainActivity.java
+++ app/src/main/java/com/example/epamandroidtest/MainActivity.java
@@ -2,20 +2,32 @@ package com.example.epamandroidtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textView = (TextView)findViewById(R.id.textView);
         Toast.makeText(getApplicationContext(), "toast feature_1", Toast.LENGTH_LONG).show();
         Toast.makeText(getApplicationContext(), "second toast feature_1", Toast.LENGTH_LONG).show();
         Toast.makeText(getApplicationContext(), "third toast feature_1", Toast.LENGTH_LONG).show();
         Toast.makeText(getApplicationContext(), "toast feature_2", Toast.LENGTH_LONG).show();
         Toast.makeText(getApplicationContext(), "second toast feature_2", Toast.LENGTH_LONG).show();
         Toast.makeText(getApplicationContext(), "third toast feature_2", Toast.LENGTH_LONG).show();
+
+
+
     }
+
+
+
+
+
+
 }
